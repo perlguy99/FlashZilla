b9b53ef800a3543ce48f324bb69183e6619225dc
Brent Michalski <Brent.Michalski@gmail.com>
2019-12-18 10:00:59 -0600

    Initial Commit

---
+++ FlashZilla/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  FlashZilla
+//
+//  Created by Michalski (US), James B on 12/18/19.
+//  Copyright © 2019 Perlguy, Inc. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
