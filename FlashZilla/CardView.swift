cafdcbd5be80a0857b6c6b7cc4077bdafb10a9df
Brent Michalski <Brent.Michalski@gmail.com>
2019-12-23 10:49:22 -0600

    Finished up tutorial project

--- FlashZilla/CardView.swift
+++ FlashZilla/CardView.swift
@@ -9,54 +9,93 @@
 import SwiftUI
 
 struct CardView: View {
+    @Environment(\.accessibilityDifferentiateWithoutColor) var differentiateWithoutColor
+    @Environment(\.accessibilityEnabled) var accessibilityEnabled
+    
     let card: Card
     var removal: (() -> Void)? = nil
     
     @State private var isShowingAnswer = false
     @State private var offset = CGSize.zero
+    @State private var feedback = UINotificationFeedbackGenerator()
     
     var body: some View {
         ZStack {
             RoundedRectangle(cornerRadius: 25, style: .continuous)
-                .fill(Color.white)
+                .fill(
+                    differentiateWithoutColor ? Color.white : Color.white.opacity(1 - Double(abs(offset.width / 50)))
+            )
+                .background(
+                    differentiateWithoutColor ? nil :
+                        RoundedRectangle(cornerRadius: 25, style: .continuous)
+                            .fill(offset.width > 0 ? Color.green : Color.red)
+            )
                 .shadow(radius: 10)
             
             VStack {
-                Text(card.prompt)
-                    .font(.largeTitle)
                 
-                if isShowingAnswer {
-                    Text(card.answer)
-                        .font(.title)
-                        .foregroundColor(.secondary)
+                if accessibilityEnabled {
+                    Text(isShowingAnswer ? card.answer : card.prompt)
+                        .font(.largeTitle)
+                }
+                else {
+                    Text(card.prompt)
+                        .font(.largeTitle)
+                    
+                    if isShowingAnswer {
+                        Text(card.answer)
+                            .font(.title)
+                            .foregroundColor(.secondary)
+                    }
                 }
             }
-        .padding(20)
-            .multilineTextAlignment(.center)
+                
+                //                Text(card.prompt)
+                //                    .font(.largeTitle)
+                //
+                //                if isShowingAnswer {
+                //                    Text(card.answer)
+                //                        .font(.title)
+                //                        .foregroundColor(.secondary)
+                //                }
+                //            }
+                
+                .padding(20)
+                .multilineTextAlignment(.center)
         }
         .frame(width: 450, height: 250)
         .rotationEffect(.degrees(Double(offset.width / 5)))
         .offset(x: offset.width * 5, y: 0)
         .opacity(2 - Double(abs(offset.width / 50)))
-    .gesture(
-        DragGesture()
-            .onChanged { gesture in
-                self.offset = gesture.translation
-        }
-        .onEnded { _ in
-            if abs(self.offset.width) > 100 {
-                // remove the card
-                self.removal?()
+        .accessibility(addTraits: .isButton)
+        .gesture(
+            DragGesture()
+                .onChanged { offset in
+                    self.offset = offset.translation
+                    self.feedback.prepare()
             }
-            else {
-                self.offset = .zero
+            .onEnded { _ in
+                if abs(self.offset.width) > 100 {
+                    // remove the card
+                    if self.offset.width > 0 {
+                        self.feedback.notificationOccurred(.success)
+                    }
+                    else {
+                        self.feedback.notificationOccurred(.error)
+                    }
+                    
+                    self.removal?()
+                }
+                else {
+                    self.offset = .zero
+                }
             }
-        }
-        
+            
         )
-        .onTapGesture {
-            self.isShowingAnswer.toggle()
+            .onTapGesture {
+                self.isShowingAnswer.toggle()
         }
+        .animation(.spring())
     }
 }
 
